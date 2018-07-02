c57218457478a740979a0fece8704c0e6dbca737
Paul Tiller <pftiller@gmail.com>
2018-07-02 13:13:01 -0500

    slight modification to server config

--- master-db.sql
+++ master-db.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE "fresh-flix";
 CREATE TABLE "users"
 (
- "user_id"  SERIAL PRIMARY KEY,
+ "id"  SERIAL PRIMARY KEY,
  "username" VARCHAR(45) NOT NULL,
  "password" VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE "watchlists"
 (
- "user_id"         INT REFERENCES "users" ("user_id"),
+ "id"         INT REFERENCES "users" ("id"),
  "movie_id"        INT NOT NULL ,
  "title"           TEXT NOT NULL ,
  "genre"           TEXT NOT NULL ,
